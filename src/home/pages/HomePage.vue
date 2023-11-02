8ab6749892c19b979066d542803d7ca7824241b1
emamaria <emascript@gmail.com>
2023-11-02 16:21:33 +0100

    home page text ok

--- src/home/pages/HomePage.vue
+++ src/home/pages/HomePage.vue
@@ -16,8 +16,12 @@ const links = [
 </header>
    
    <div class="container">
-    <h1>Home</h1>
-
+     <main class="app_explanation_container">
+       <h1>Give one advice about anything you want!</h1>
+       <p>For example: Better have Ceylon Cinnamon than Cassia, is less toxic for your liver, anyway don`t abuse any of it.</p>
+        
+    </main>
+   
     
     </div>
 </template>
@@ -25,8 +29,28 @@ const links = [
 
 <style scoped>
 
+
+ h1{
+    color: white;
+    font-size: 7rem;
+    
+ }
+
+ p{
+    width: 70%;
+    margin-top: 30px;
+    font-size: 2rem;
+    padding: 2rem;
+    color: #d7cebb;
+ }
  .container{
     background-color: #6e5064;
     min-height: 100vh;
+   
+ }
+
+ .app_explanation_container{
+    width: 90%;
+    padding: 10rem;
  }
 </style>
