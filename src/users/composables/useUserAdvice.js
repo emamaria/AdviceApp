fc36ca4156ade41ad55572537ecf7106436d525e
emamaria <emascript@gmail.com>
2023-12-15 13:17:01 +0100

    fixed useUserAdvice composable for the case there is not user advice un db

--- src/users/composables/useUserAdvice.js
+++ src/users/composables/useUserAdvice.js
@@ -28,13 +28,30 @@ const store = useAdviceStore()
                     
   const userId = userStore.userData.uid
   
- 
+ //miro si el usuario registrado ya tiene un advice
   
-  const authUserAdviceId = allAdvice.advise.filter(advice => advice.userId._id === userId)[0]._id
+  const authUserAdviceId = allAdvice.advise.filter(advice => advice.userId?._id === userId)[0]?._id
+
+
+  console.log(authUserAdviceId, "id del advice de usuario registrado")
+
+  //si el usuario no tiene advice hago el set del dato del store solo con dato de usuario id, name e email
+  if(authUserAdviceId === undefined){
+    store.setAdvice({
+        _id: "",
+        advice: "",
+        img: "",
+        userId: userStore.userData
+        
+    })
+
+    
+}
 
 
 const getAuthUserAdvice = async() => {
 
+    
     const {data} = await userApi.get(`/advice/${authUserAdviceId}`)
    
     return data
@@ -42,36 +59,47 @@ const getAuthUserAdvice = async() => {
 
 const useUserAdvice = () => {
 
-    
+    //si el usuario tiene advice ejecuto la peticion para obtener advice y retorno el advice ,loading, error etc
 
-    const {isLoading, data, error, isError} = useQuery({
-        queryKey: ['userAdvice', authUserAdviceId],
-        queryFn: getAuthUserAdvice,
-        // staleTime: 1000 * 60 * 60 * 24
-    })
+    if(authUserAdviceId){
+
+        const {isLoading, data, error, isError} = useQuery({
+            queryKey: ['userAdvice', authUserAdviceId],
+            queryFn: getAuthUserAdvice,
+            // staleTime: 1000 * 60 * 60 * 24
+        })
+    
+       
+        
+    
+        watch(data, advice => {
+            if(advice){
+                store.setAdvice(advice) 
+               
+            }
+                           
+        }, {immediate: true})
+
+
+        return {
+            userAuthAdvice,
+            isLoading,
+            error,
+            isError,
+           
+           }
+    
+    }else{
+        return {
+            //si el usuario no tiene advice hecho retorno el advice vacío solo con datos del usuario name, email, id
+            userAuthAdvice  
+        }
+    }
 
    
-    
-
-    watch(data, advice => {
-        if(advice){
-            store.setAdvice(advice) 
-           
-        }
-                       
-    }, {immediate: true})
-
  
 
-    return {
-        userAuthAdvice,
-        isLoading,
-        error,
-        isError,
-       
-       
-    
-    }
+   
 }
 
 export default useUserAdvice
