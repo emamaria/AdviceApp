6d588a7d439e0137279dc0b26e38fe18f5f616c3
emamaria <emascript@gmail.com>
2023-12-31 12:52:34 +0100

    check if both passwords match when updating user data

--- src/users/pages/PersonalPage.vue
+++ src/users/pages/PersonalPage.vue
@@ -1,11 +1,22 @@
 <script setup>
 import usePersonalReq from '../composables/usePersonalReq';
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 
 const isLoading = ref(false)
 const isReqResponse = ref(false)
 const reqResponseMessage = ref("")
+
+const passwordInValid = computed( () => {
+    if(form.value.password1 !== form.value.password2 ){
+        if(form.value.password2.length !== 0 && form.value.password1.length !== 0){
+             return true
+        }
+        
+    }
+
+    return false
+})
 
 const {showUserData, updateUser} = usePersonalReq(isLoading, isReqResponse, reqResponseMessage)
 
@@ -22,6 +33,10 @@ const updatingPersData = async() => {
     if(form.value.email === "" && form.value.name === "" && form.value.password1 === ""){
         return
     } 
+
+    if(form.value.password1 !== form.value.password2){
+        return
+    }
 
     console.log(form.value.name, form.value.email, form.value.password1)
 
@@ -49,6 +64,7 @@ const updatingPersData = async() => {
        <input type="email" v-model="form.email" placeholder="email">
        <input type="password" v-model="form.password1" placeholder="password">
        <input type="password" v-model="form.password2" placeholder="password">
+       <small v-if="passwordInValid">passwords don´t match</small>
        <input type="submit" value="submit">
        <small v-if="isLoading">loading...</small>
        <small v-if="isReqResponse">{{ reqResponseMessage }}</small>
